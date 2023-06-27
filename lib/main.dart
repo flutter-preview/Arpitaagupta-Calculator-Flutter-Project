5142f75dbd703bbcdcda406484201acd755c0473
Arpita Gupta <arpitagupta6620@gmail.com>
2023-06-28 00:57:19 +0530

    Completed the basic calculator app using flutter-dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calculator/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,13 +16,92 @@ class CalculatorApp extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<CalculatorApp> {
+  //variables
+  double firstNum = 0.0;
+  double secondNum = 0.0;
+  var input = '';
+  var output = '';
+  var operation = '';
+  var hideInput = false;
+  var outputSize = 34.0;
+
+  onButtonClick(value) {
+    //if value is AC
+
+    if (value == "AC") {
+      //reset
+      input = '';
+      output = '';
+    } else if (value == "<-") {
+      if (input.isNotEmpty) {
+        input = input.substring(0, input.length - 1);
+      }
+    } else if (value == "=") {
+      if (input.isNotEmpty) {
+        var userInput = input;
+        userInput = input.replaceAll("x", "*");
+        Parser p = Parser();
+        Expression expression = p.parse(userInput);
+        ContextModel cm = ContextModel(); //initialization of Context Model
+        var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+        output = finalValue.toString();
+        if (output.endsWith(".0")) {
+          output = output.substring(0, output.length - 2);
+        }
+        input = output;
+        hideInput = true;
+        outputSize = 52;
+      }
+    } else {
+      input = input + value;
+      hideInput = false;
+      outputSize = 34;
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: [
-          Expanded(child: Container(color: Colors.red)),
+          //input output area
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    hideInput ? '' : input,
+                    style: TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    //to add space
+                    height: 20,
+                  ),
+                  Text(
+                    output,
+                    style: TextStyle(
+                      fontSize: outputSize,
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                  const SizedBox(
+                    //to add space
+                    height: 30,
+                  )
+                ],
+              ),
+            ),
+          ),
+
           //buttons area,
           Row(
             children: [
@@ -97,11 +177,11 @@ class _MyWidgetState extends State<CalculatorApp> {
                   borderRadius: BorderRadius.circular(12)),
               padding: EdgeInsets.all(22),
               primary: buttonbgColor),
-          onPressed: () {},
+          onPressed: () => onButtonClick(text),
           child: Text(
             text,
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 22,
               color: tColor,
               fontWeight: FontWeight.bold,
             ),
